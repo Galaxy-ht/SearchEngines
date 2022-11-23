02f98111d92745abcc052318a2445f96cf7b382d
lodge <17870039410@sina.cn>
2022-11-23 13:45:36 +0800

    feat:添加搜索界面

--- code/search-engine-web/src/views/homeView.vue
+++ code/search-engine-web/src/views/homeView.vue
@@ -1,21 +1,36 @@
 <template>
-  <el-main style="height: 100vh; text-align: center;">
-    <el-input v-model="searchInput"
-              style="width: 600px; height: 60px; font-size: 18px; margin: 20px;"
-              placeholder="搜索内容">
-      <template #suffix>
-        <el-button type="text" @click="do_search">
-          <el-icon size="22">
-            <Microphone/>
-          </el-icon>
-        </el-button>
-        <el-button type="text">
-          <el-icon size="22">
-            <search/>
-          </el-icon>
-        </el-button>
-      </template>
-    </el-input>
+  <el-main >
+    <el-row :gutter="16">
+      <el-col :span="2"></el-col>
+      <el-col :span="14">
+        <el-card style="min-height: 60vh; text-align: center;">
+          <el-input v-model="searchInput"
+                    style="width: 700px; height: 50px; font-size: 18px; margin: 20px;"
+                    placeholder="搜索内容">
+            <template #suffix>
+              <el-button type="text" @click="do_search">
+                <el-icon size="22">
+                  <Microphone/>
+                </el-icon>
+              </el-button>
+              <el-button type="text">
+                <el-icon size="22">
+                  <search/>
+                </el-icon>
+              </el-button>
+            </template>
+          </el-input>
+          <el-empty description="输入搜索内容" v-if="!searchInput.length"></el-empty>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>
+          <template #header>
+            <span style="font-size: 20px; font-weight: 600;">热榜</span>
+          </template>
+        </el-card>
+      </el-col>
+    </el-row>
   </el-main>
 </template>
 
